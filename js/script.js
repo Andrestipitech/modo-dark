d0c3e830875b768d25aa3a58c20fc9bae98930ee
Andrestipitech <119084933+Andrestipitech@users.noreply.github.com>
2022-11-25 18:51:06 -0500

    inicio

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$('#listo').hide();
+
+$(document).ready(function(){
+    
+    $('#nolisto').fadeOut(2000, function(){$('#listo').fadeIn(4000);});
+
+    
+    $('#desaparecer').click(function(){
+        $("#logo").hide("slow");
+    });
+
+    $('#aparecer').dblclick(function(){
+        $("#logo").show("slow");
+    });
+
+    $('#down-up').click(function(){
+        $('#logo').slideDown("slow");
+    });
+
+    $("#pa2").css({"background-color":"red"});
+
+    $('.segundo').mouseenter(function(){
+        $('.tercero').hide("slow");
+        $('.primero').animate({width: "300px"});
+    });
+
+    $('.segundo').mouseleave(function(){
+        $('.tercero').show("slow");
+    });
+    
+    $('#listo').click(function(){
+        //alert($("#pa4").text());
+        //$("#pa4").text("python")
+        //$("#pa4").append(" python")
+        //$("#pa4").prepend("python <strong> Adios </strong>") 
+        //$("#pa4").after(" <p>parrafo insertado con Jquery</p>")
+        $("#pa4").before(" <p>parrafo insertado con Jquery</p>")
+    });
+
+    /*$('#eliminar').click(function(){
+        $('#pa1').remove()
+    });*/
+    $('#eliminar').click(function(){
+        $('#listo').empty();
+        //$('#titulo').addClass("red");
+        $('#titulo').toggleClass("verde");
+    });
+
+});
+
